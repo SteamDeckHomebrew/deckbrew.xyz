15f24ed8ff3cbc1c142201cf6677a9cbed098576
EMERALD0874 <hudson.samuels@gmail.com>
2023-01-22 10:26:16 -0600

    Add fontsource italics and bolds

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,2 +1,6 @@
-import "@fontsource/roboto";
-import "@fontsource/montserrat";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/400-italic.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/700-italic.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/700.css";
